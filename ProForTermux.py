0053c6fee5645209acc55edd9e22b23c22af0c6a
profortermux1 <profortermux2020>
2020-10-03 14:45:57 +0100

    Hy This a New Tool By Pro For Termux

---
+++ ProForTermux.py
@@ -0,0 +1,314 @@
+import os,sys,time
+
+os.system("clear")
+
+# Banner "Slow Index Logo Pro For Termux"
+
+from V7xStyle import Style
+
+from pyfiglet import Figlet #Animation Figlet
+
+def render(text,style):
+        	f = Figlet(font=style)
+        	print(f.renderText(text))
+
+#V7xStyle
+
+from V7xStyle import Animation
+
+An = Animation
+
+#TypeWriter
+
+def jalan(z) :
+	for e in z + '\n' :
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.06)
+
+text='''              
+             \033[1;34m___                ______    
+            \033[1;32m/ __ \________     / ____/__    ____ 
+          \033[1;31m / /_/ / ___/ __ \   / /_  / __ \/ ___/
+        \033[1;30m / ____/ /  / /_/ /  / __/ / /_/ / /
+       \033[1;33m/_/   /_/   \____/  /_/    \____/_/'''  
+       
+time.sleep(0.3)
+
+An.SlowIndex(text,t=0.01)
+
+time.sleep(0.3)
+
+text2='''
+  \033[1;34m ______     
+ \033[1;32m/_  _ _/__  _________ ___  __  ___  __        
+   \033[1;31m / / / _ \/ ___/ __ `__ \/ / / / |/_/
+   \033[1;35m/ / /  __/ /  / / / / / / /_/ />  <
+  \033[1;30m/_/  \___/_/  /_/ /_/ /_/\__,_/_/|_|'''
+
+time.sleep(0.3)
+
+An.SlowIndex(text2,t=0.01)
+
+time.sleep(0.3)
+
+print("\n")
+
+S = "Created By"
+
+time.sleep(0.3)
+
+render(S,'bubble')
+
+S = Style('═╬════════►    Pro For Termux   ',).Square()
+
+time.sleep(0.3)
+
+print(S)
+
+print('\n')
+
+
+from getpass import getpass
+time.sleep(0.4)
+user = input("\033[1;35m═>\033[1;36m Enter Your Name: ")
+
+print("\n")
+
+########################################################
+textD='''
+
+       ▄▀▀▀▄
+  █    █
+ ███████         ▄▀▀▄
+░██─▀─██░░█▀█▀▀▀▀█░░█░      \033[1;31mpassowrd \033[1;32mcorrect!
+░███▄███░░▀░▀░░░░░▀▀░░
+'''
+##########################################################
+textP='''
+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
+ |p| |a| |s| |s| |o| |w| |r| |d| = \033[1;31mPro \033[1;33mFor \033[1;32mTermux
+ +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+'''
+         
+####################################################
+
+time.sleep(0.3)
+print(textP)
+time.sleep(0.3)
+print('\n')
+while True :	
+    
+    passowd = getpass("\033[1;33m══> \033[1;34m Enter Passowrd: ")
+
+    if passowd == "Pro For Termux" :
+       print(textD)
+       time.sleep(0.3)
+       print('\n')
+       render('Subscribe','bubble')
+       time.sleep(0.3)
+       render('Now Please','bubble')
+       time.sleep(0.3)
+       print('\n')
+       time.sleep(0.3)
+       An.Text(AT='Opening link Channel.........' ,text='',CLT='\033[1;33m')
+       break
+    else :
+       print("\n")
+       An.Text(AT='Passowrd Faild Try Again Please',text='',repeat=1)
+       print("\n")
+####################################################################
+We='''
+                            \033[1;36m▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
+                            █▒▒░░░░░░░░░▒▒█
+                             █░░█░░░░░█░░█
+                          █    ░░░▀█▀░░░█ ▄▄
+                        █░░█▀▄░░░░░░░▄▀─█░░█
+                       █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+                     █----╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗----█
+                     █----║║║╠─║─║─║║║║║╠─----█
+                     █----╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝----█
+                       █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+
+                          \033[1;34m╔════╗╔═══╗
+                          \033[1;32m║╔╗╔╗║║╔═╗║
+                         \033[1;31m ╚╝║║╚╝║║ ║║
+                            \033[1;35m║║  ║║ ║║
+                            \033[1;30m║║  ║╚═╝║
+                            \033[1;33m╚╝  ╚═══╝
+
+      \033[1;34m╔═══╗╔══╗╔═╗╔═╗    ╔════╗╔═══╗╔═══╗╔═╗╔═╗╔╗ ╔╗╔═╗╔═╗
+      \033[1;32m║╔══╝╚╣─╝╚╗╚╝╔╝    ║╔╗╔╗║║╔══╝║╔═╗║║║╚╝║║║║ ║║╚╗╚╝╔╝
+      \033[1;33m║╚══╗ ║║  ╚╗╔╝      ╚ ║║╚╝║╚══╗║╚═╝║║╔╗╔╗║║║ ║║ ╚╗╔╝
+      \033[1;35m║╔══╝ ║║  ╔╝╚╗        ║║  ║╔══╝║╔╗╔╝║║║║║║║║ ║║ ╔╝╚╗
+      \033[1;30m║║   ╔╣─╗╔╝╔╗╚╗       ║║  ║╚══╗║║║╚╗║║║║║║║╚═╝║╔╝╔╗╚╗
+      \033[1;31m╚╝   ╚══╝╚═╝╚═╝       ╚╝  ╚═══╝╚╝╚═╝╚╝╚╝╚╝╚═══╝╚═╝╚═╝
+
+'''
+#####################################################
+print('\n')
+time.sleep(0.3)
+print('\n')
+jalan("\033[1;31mPlease Subscribe in Channel ")
+os.system('xdg-open https://m.youtube.com/channel/UCkHzOCgII8a8KQRf68sHUxg')
+os.system("clear")
+print(We)
+####################################################################
+def main():
+    print("\033[1;31m[1] \033[1;34mTo Start")
+    time.sleep(0.3)
+    print('\n')
+    print("\033[1;31m[0] \033[1;34mTo Exit")
+###################################################################
+print('\n')
+main()
+
+print('\n')
+
+time.sleep(0.3)
+
+
+choose = input("==>\033[1;37mchoose an option : ")
+if choose == '1' :
+    print('\n')
+    time.sleep(0.3)
+    An.DL(text='B#Proccessing',t=0.1,width=50)
+    os.system("clear")
+    jalan("   Wait To install some package ............................")
+    print("\n")
+    print("\n")
+    jalan('\033[1;37m**************************Staring**********************************')
+
+    print('\n')
+    
+    render('Update','banner3-D')
+    
+    print('\n')
+
+    os.system('apt update')
+
+    print('\n')
+
+    render('Ugrade','banner3-D')
+
+    print('\n')
+
+    os.system('apt upgrade')
+
+    print('\n')
+
+    render('Git','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install git')
+
+    print('\n')
+
+    render('Python','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install python')
+
+    print('\n')
+
+    render('Python2','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install python2')
+
+    print('\n')
+
+    render('PHP','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install php')
+
+    print('\n')
+
+    render('Wget','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install wget')
+
+    print('\n')
+
+    render('Curl','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install curl','banner3-D')
+
+    print('\n')
+
+    render('SSH','banner3-D')
+
+    print('\n')
+
+    render('OpenSSH','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install openssh')
+
+    print('\n')
+
+    render('Tor','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install tor')
+
+    print('\n')
+
+    render('future''banner3-D')
+
+    print('\n')
+
+    os.system('pkg install future')
+
+    print('\n')
+
+    render('ruby','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install ruby')
+
+    print('\n')
+
+    render('coloroma','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install colorama')
+
+    print('\n')
+
+    render('Zip','banner3-D')
+
+    print('\n')
+
+    os.system('pkg install zip && pkg unzip')
+
+    print('\n')
+
+    render('Thanck You'+user,'banner3-D')
+
+    print("Thanck you"+user)
+   
+    print('Thanck you For Usinf Tool By!!'+user)
+
+    S = Style('Created By Pro For Termux ').Square(Equal=True)
+
+    os.system("exit")
+
+###################################################################
+elif choose == '0' :
+    S = Style('Created By Pro For Termux ').Square(Equal=True)
+    os.system("exit")
+###################################################################
